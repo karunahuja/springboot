1eb90e838b4639459f3dd6e4da62cd588de220d2
karun <karunahuja1998@gmail.com>
2020-09-15 07:20:49 +0530

    first commit

---
+++ src/main/java/com/springboot/web/controller/MainController.java
@@ -0,0 +1,28 @@
+package com.springboot.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+	
+	@RequestMapping("/")
+	public String home() {
+		
+		System.out.println("home page");
+		return "home";
+	}
+	
+	
+	@RequestMapping("/contact")
+	public String contact() {
+		
+		System.out.println("home page");
+		return "contact";
+	}
+	
+	
+	
+}
